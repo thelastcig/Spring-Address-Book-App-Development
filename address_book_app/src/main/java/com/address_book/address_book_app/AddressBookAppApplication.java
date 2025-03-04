0a80868e97e369bbbb19af5d6be755538fae31d3
thelastcig <sonusharmaworkspace@gmail.com>
2025-03-04 12:05:44 +0530

    [Sonu Sharma] Add. Address Book Spring project setup

---
+++ address_book_app/src/main/java/com/address_book/address_book_app/AddressBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.address_book.address_book_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+
+}
---
+++ address_book_app/src/test/java/com/address_book/address_book_app/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.address_book.address_book_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
